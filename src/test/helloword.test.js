85bde58f0180e26c390d59bd7574fa51769f71cc
miku <1803770129@qq.com>
2021-04-08 23:12:29 +0800

    获取被connect包裹的类

--- src/test/helloword.test.js
+++ src/test/helloword.test.js
@@ -1,18 +1,18 @@
+import SYS from '../pages/system/systemfn';
 import PART from '../pages/system/systemfn/spart';
 import React from 'react';
 import Enzyme,{shallow,mount} from 'enzyme';
 import { connect } from 'dva';
 import Adapter from 'enzyme-adapter-react-16';
+import store from '../models/statistics';
 Enzyme.configure({ adapter: new Adapter() });
 it('Systems', () => {
-  const mapStateToProps = (state) => {
-    return {
-      loading:state.loading.effects,
-      ...state.statistics,
-    };
-  
-  };
-  const wrapper = mount(connect(mapStateToProps)(PART));
-  console.log(wrapper);
-      
+  const Parts = PART;
+  const s=SYS;
+  const COMPONENT=Parts.WrappedComponent; 
+  // console.log(s); 
+  // const wrapper = mount(<COMPONENT loading={store.namespace+'/'+store.effects.fetchPartList} />);
+  const wrapper = mount(<COMPONENT store={store} loading={store.effects.fetchPartList} />);
+  // const wrapper = mount(<SYS />);
+  console.log(...wrapper);
 });
